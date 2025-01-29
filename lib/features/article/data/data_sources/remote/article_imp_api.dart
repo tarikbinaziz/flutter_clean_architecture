90dcf690d0f939c6c446af24808997e4de28a820
tarik <mdtarikbinaziz@gmail.com>
2025-01-29 10:35:24 +0600

    error solve

--- lib/features/article/data/data_sources/remote/article_imp_api.dart
+++ lib/features/article/data/data_sources/remote/article_imp_api.dart
@@ -2,12 +2,12 @@ import 'package:clean_architecture/features/article/data/data_sources/remote/abs
 import 'package:clean_architecture/features/article/domain/model/article_model/result.dart';
 import 'package:dio/dio.dart';
 
-class ArticleImpApi extends AbstractArticleApi {
-  final Dio dio;
-  ArticleImpApi({required this.dio});
+// class ArticleImpApi extends AbstractArticleApi {
+//   final Dio dio;
+//   ArticleImpApi({required this.dio});
 
-  // article method
-  @override
-  Future <ApiResponse<List<Result>>> getArticles() {
-  }
-}
+//  // // article method
+//   // @override
+//   // Future <ApiResponse<List<Result>>> getArticles() {
+//   // }
+// }
